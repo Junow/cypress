afccc8c1c4e481560abf12507c81515c4dcbe97b
junow <taristmas@gmail.com>
2020-02-10 17:12:04 +0900

    [Modified] modify test

--- cypress/integration/NaverShopping.spec.js
+++ cypress/integration/NaverShopping.spec.js
@@ -2,7 +2,8 @@
 /// <reference types="Cypress" /
 
 describe('네이버쇼핑', function() {
-    it('네이버쇼핑 - "코로나" 검색', function() {
+
+    it('Naver Shopping', function() {
       cy.visit('https://shopping.naver.com/home/p/index.nhn')
       cy.get('[_clickcode="search"]').as('searchButton') // 검색 버튼
 
@@ -14,11 +15,47 @@ describe('네이버쇼핑', function() {
       cy.get('@searchButton')
         .click()
 
-      cy.get('#_queryExplain > em')
-        .invoke('text', '코로나')
-        
 
-      cy.screenshot(`${Date.now()}`)
-        
+
+      cy.url()
+        .should('eq', 'https://search.shopping.naver.com/search/all.nhn?query=%EC%BD%94%EB%A1%9C%EB%82%98&cat_id=&frm=NVSHATC')
+
+      
+
+      cy.screenshot(`${this.title}`)
+
+    })
+
+    it('search result', () => {
+      cy.visit('https://search.shopping.naver.com/search/all.nhn?query=%EC%BD%94%EB%A1%9C%EB%82%98&cat_id=&frm=NVSHATC')
+      cy.url()
+        .should('eq', 'https://search.shopping.naver.com/search/all.nhn?query=%EC%BD%94%EB%A1%9C%EB%82%98&cat_id=&frm=NVSHATC')
+      cy.get('#_priceApplyButton').as('priceSearchButton') // 가격 검색 버튼
+
+        cy.get('#_queryExplain > em')
+        .invoke('text', '코로나')
+
+      cy.get('[data-nclick-area="rcc*C"]')
+        .click()
+
+      cy.get('#_filter > div')
+        .first()
+        .should('have.class', 'on')
+
+      const min_p = 4
+      const max_p = 3
+
+      cy.get('#min_p')
+        .type(min_p)
+      cy.get('#max_p')
+        .type(max_p)
+
+      cy.get('@priceSearchButton')
+        .click()
+
+      cy.get('[data-nclick-code="rcc.reset"]')
+        .invoke('text', `${min_p<max_p?min_p: max_p} ~ ${min_p<max_p?max_p:min_p}`)
+
+      cy.screenshot(`${this.title}`)
     })
   })  
